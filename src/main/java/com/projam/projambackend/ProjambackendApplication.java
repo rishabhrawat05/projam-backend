4c62c78643a92622bcd4ce20769794a3a8d8c07e
Rishabh <rishabhrawat.171105@gmail.com>
2025-05-03 14:00:58 +0530

    Projam Backend

---
+++ src/main/java/com/projam/projambackend/ProjambackendApplication.java
@@ -0,0 +1,13 @@
+package com.projam.projambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjambackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjambackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projam/projambackend/ProjambackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projam.projambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjambackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
